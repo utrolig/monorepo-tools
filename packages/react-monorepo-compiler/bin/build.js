e0e31668e63fae4800a45b4753c4897fb9cac093
Stian Bakken <sbakken@gmail.com>
2019-06-21 08:12:28 +0200

    Updated ignore paths and removed files from package.json

--- packages/react-monorepo-compiler/bin/build.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-function buildApplication() {
-    console.log("Building application");
-}
-exports.buildApplication = buildApplication;
--- packages/react-monorepo-compiler/bin/src/test.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-function runTests() {
-    console.log("Running tests...");
-}
-exports.runTests = runTests;
