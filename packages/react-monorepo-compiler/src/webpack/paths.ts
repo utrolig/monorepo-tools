22780ec442e6693aeb7f1759f740066577ea266b
Stian Bakken <sbakken@gmail.com>
2019-06-24 07:28:56 +0200

    Added more paths.

--- packages/react-monorepo-compiler/src/webpack/paths.ts
+++ packages/react-monorepo-compiler/src/webpack/paths.ts
@@ -5,7 +5,9 @@ export const currentAppDirectory = process.cwd();
 export const appNodeModules = path.join(currentAppDirectory, "node_modules");
 export const buildFolder = path.join(currentAppDirectory, "build");
 export const srcFolder = path.join(currentAppDirectory, "src");
-export const templatePath = path.join(currentAppDirectory, "public/index.html");
+export const publicFolder = path.join(currentAppDirectory, "public");
+export const templatePath = path.join(publicFolder, "index.html");
+export const tsConfig = path.join(currentAppDirectory, "tsconfig.json");
 export const getAliasPaths = () => {
   const packagesFolder = path.join(currentAppDirectory, "../");
   const folders = fs.readdirSync(packagesFolder);
