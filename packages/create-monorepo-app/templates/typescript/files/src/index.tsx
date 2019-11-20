cd46946d5385fb3ef196e7538d25a6a3f917c8d5
Dmitry Koujelev <dkoujelev97@gmail.com>
2019-11-20 22:59:33 +0100

    cleanup

--- packages/create-monorepo-app/templates/typescript/files/src/index.tsx
+++ packages/create-monorepo-app/templates/typescript/files/src/index.tsx
@@ -1,11 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import pkgJson from "../package.json";
-import { Root } from "./root";
+import App from "./App";
 
 function render() {
-  const element = document.getElementById("page");
-  ReactDOM.render(<Root title={pkgJson.name} />, element);
+  const el = document.getElementById("page");
+  ReactDOM.render(<App />, el);
 }
 
 render();
