d6a90f5e0dae072026381114236ac93df816310e
Dmitry Koujelev <dkoujelev97@gmail.com>
2019-11-22 19:06:45 +0100

    update index files

--- packages/create-monorepo-app/templates/reduxjs/files/src/index.js
+++ packages/create-monorepo-app/templates/reduxjs/files/src/index.js
@@ -2,8 +2,4 @@ import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
 
-const _render = el => {
-  ReactDOM.render(<App />, el);
-};
-
-window.renderReactReduxJsApp = _render;
+ReactDOM.render(<App />, document.getElementById("page"));
--- packages/create-monorepo-app/templates/typescript/files/src/index.tsx
+++ packages/create-monorepo-app/templates/typescript/files/src/index.tsx
@@ -2,12 +2,4 @@ import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
 
-type WindowType = typeof window & {
-  renderReactTsApp: (el: HTMLElement) => void;
-};
-
-const _render = (el: HTMLElement): void => {
-  ReactDOM.render(<App />, el);
-};
-
-(window as WindowType).renderReactTsApp = _render;
+ReactDOM.render(<App />, document.getElementById("page"));
